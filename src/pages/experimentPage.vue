be92fc57649e795c65ccf9096bdb1d43c4c6f8a3
michael angelo <mikocoral05@gmail.com>
2025-05-13 20:45:30 +0800

    experimetn page

---
+++ src/pages/experimentPage.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <TextClamp :max-lines="2" :text="text">
+      <template #after="{ toggle, expanded }">
+        <button @click="toggle">
+          {{ expanded ? "Show less" : "Show more" }}
+        </button>
+      </template>
+    </TextClamp>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+import TextClamp from "vue3-text-clamp";
+
+export default defineComponent({
+  components: {
+    TextClamp,
+  },
+  setup() {
+    const text =
+      "hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world ";
+
+    return {
+      text,
+    };
+  },
+});
+</script>

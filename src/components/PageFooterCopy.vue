746903beb9b384b8cc46a7114edac0494ca5f5fb
michael angelo <mikocoral05@gmail.com>
2025-05-14 19:53:56 +0800

    copy of pagefooter

---
+++ src/components/PageFooterCopy.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="container">
+<div class="first_text"><i>Looking for unconditional love? Visit the shelter today!</i></div>
+<div class="tabaco_footer"><h5 class="q-mt-sm q-mb-md q-ml-md"><b>Tabaco Animal Rescue and Adoption</b></h5></div>
+<div class="lower_footer q-pl-md">
+  <div class="flex column">
+<p>Connect With Us</p>
+<p><q-icon size="25px" name="call" /> 0905-523-8105</p>
+<p><q-icon size="25px" name="place" /> P-3 Burac St., San Lorenzo, Tabaco, Philippines</p>
+<div><q-icon size="25px" name="mail" /><q-icon size="25px" name="facebook" /><q-icon size="25px" name="instagram" /><q-icon size="25px" name="tiktok" /></div>
+  </div>
+  <div class="flex column">
+    <p><b>Gcash Number:</b> 0905-523-8105</p>
+    <p><b>BDO:</b> 005150343294</p>
+    <p><b>PayPal:</b> 0905-523-8105</p>
+    <p><b>Account Name:</b> EDNALYN C.</p>
+  </div>
+  <div class="flex column q-mr-md">
+<p class="footer_right_side" >Help Us Rescue More Animals <br> Be A Volunteer Today!</p>
+<q-btn class="button_join_us" flat>Join Us</q-btn>
+  </div>
+</div>
+</div>
+</template>
+<script>
+import { defineComponent } from 'vue'
+export  default defineComponent ({
+  name:"PageFooterCopy",
+
+  setup(){
+
+    return{
+
+
+    };
+  },
+});
+</script>
+<style lang="scss" scoped>
+@import "../pages/taara/style/PageFooter.scss";
+</style>

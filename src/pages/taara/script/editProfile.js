fa14aa8c6d3fca1814dcc4152125749544a48a6a
michael angelo <mikocoral05@gmail.com>
2025-05-14 19:56:07 +0800

    edit profile js update

--- src/pages/taara/script/editProfile.js
+++ src/pages/taara/script/editProfile.js
@@ -1,24 +1,24 @@
-import { ref } from "vue";
+import { ref } from 'vue'
 import {
   logInDetails,
   updatePublicUserDetails,
   userSettingChanger,
-} from "../../../composable/taaraComposable";
-import { resizeImage } from "src/composable/simpleComposable";
+} from 'src/composable/taaraComposable'
+import { resizeImage } from 'src/composable/simpleComposable'
 // import { QSpinnerGears, useQuasar } from "quasar";
-import PageFooter from "../../../components/PageFooter.vue";
+import PageFooter from 'src/components/PageFooter.vue'
 export default {
   components: {
     PageFooter,
   },
   setup() {
     // const $q = useQuasar();
-    let changer = ref(userSettingChanger);
-    let read = ref(userSettingChanger.value);
+    let changer = ref(userSettingChanger)
+    let read = ref(userSettingChanger.value)
 
-    changer.value = JSON.parse(localStorage.getItem("changer"));
-    let logInDataCopy = { ...logInDetails.value[0] };
-    console.log(logInDetails.value[0]);
+    changer.value = JSON.parse(localStorage.getItem('changer'))
+    let logInDataCopy = { ...logInDetails.value[0] }
+    console.log(logInDetails.value[0])
     let userdata = ref({
       first_name: null,
       middle_name: null,
@@ -37,101 +37,101 @@ export default {
       valid_id: null,
       valid_id_name: null,
       image: null,
-    });
-    userdata.value = logInDataCopy;
+    })
+    userdata.value = logInDataCopy
     let update = (payload) => {
-      updatePublicUserDetails(payload);
-    };
+      updatePublicUserDetails(payload)
+    }
     let cancel = () => {
-      userdata.value = { ...logInDetails.value[0] };
-    };
+      userdata.value = { ...logInDetails.value[0] }
+    }
 
     const handleFileUpload = (event, param) => {
-      const files = event.target.files;
-      const file = files[0];
-      const reader = new FileReader();
-      reader.readAsDataURL(file);
+      const files = event.target.files
+      const file = files[0]
+      const reader = new FileReader()
+      reader.readAsDataURL(file)
       reader.onload = () => {
         resizeImage(file, 500, 500)
           .then(({ dataUrl, fileName }) => {
-            if (param == "profileImage") {
-              userdata.value.image = dataUrl;
+            if (param == 'profileImage') {
+              userdata.value.image = dataUrl
             } else {
-              userdata.value.valid_id = dataUrl;
-              console.log(fileName);
-              userdata.value.valid_id_name = fileName;
+              userdata.value.valid_id = dataUrl
+              console.log(fileName)
+              userdata.value.valid_id_name = fileName
             }
           })
           .catch((error) => {
-            console.error(error);
-          });
-      };
-    };
+            console.error(error)
+          })
+      }
+    }
 
-    const fileInput = ref(null);
+    const fileInput = ref(null)
     const pickFile = () => {
-      document.getElementById("fileInput").click();
-    };
+      document.getElementById('fileInput').click()
+    }
 
     let imageShow2 = () => {
-      document.getElementById("file2").click();
-    };
+      document.getElementById('file2').click()
+    }
 
     return {
       imageShow2,
       handleFileUpload,
       civil_status_options: [
         {
-          label: "Single",
+          label: 'Single',
           value: 1,
         },
         {
-          label: "Married",
+          label: 'Married',
           value: 2,
         },
         {
-          label: "Seperated",
+          label: 'Seperated',
           value: 3,
         },
       ],
       sex_options: [
         {
-          label: "Male",
+          label: 'Male',
           value: 1,
         },
         {
-          label: "Female",
+          label: 'Female',
           value: 2,
         },
       ],
       suffix_options: [
         {
-          label: "none",
-          value: "none",
+          label: 'none',
+          value: 'none',
         },
         {
-          label: "Jr",
-          value: "Jr",
+          label: 'Jr',
+          value: 'Jr',
         },
         {
-          label: "Sr",
-          value: "Sr",
+          label: 'Sr',
+          value: 'Sr',
         },
         {
-          label: "||(second)",
-          value: "||",
+          label: '||(second)',
+          value: '||',
         },
         {
-          label: "Jd(Juris Doctor)",
-          value: "Jd",
+          label: 'Jd(Juris Doctor)',
+          value: 'Jd',
         },
         {
-          label: "MBA(Master in Business Administration)",
-          value: "MBA",
+          label: 'MBA(Master in Business Administration)',
+          value: 'MBA',
         },
         {
-          label: "Ph.D(Philosophical Doctor)",
-          value: "Ph.D",
+          label: 'Ph.D(Philosophical Doctor)',
+          value: 'Ph.D',
         },
       ],
       pickFile,
@@ -142,6 +142,6 @@ export default {
       read,
       logInDetails,
       changer,
-    };
+    }
   },
-};
+}

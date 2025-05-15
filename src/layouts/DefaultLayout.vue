3e34ae4289603f6ffc22e41621ee58784e3df881
michael angelo <mikocoral05@gmail.com>
2025-05-15 19:41:53 +0800

    new defaultl aout

---
+++ src/layouts/DefaultLayout.vue
@@ -0,0 +1,19 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-page-container>
+      <router-view class="q-pa-lg" />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'MainLayout',
+
+  setup() {
+    return {}
+  },
+})
+</script>

f6411cc5bd0475b16ed70e4f085aae55991ce00d
michael angelo <mikocoral05@gmail.com>
2025-05-18 18:09:37 +0800

    add getting of pending for olvunteer

--- src/boot/initialLoad.js
+++ src/boot/initialLoad.js
@@ -1,10 +1,15 @@
 import { defineBoot } from '#q-app/wrappers'
-import { getPendingRescueReport } from 'src/composable/latestComposable'
+import { getPendingRescueReport, getPendingVolunteer } from 'src/composable/latestComposable'
 import { globalStore } from 'src/stores/global-store'
 const store = globalStore()
 export default defineBoot(() => {
   getPendingRescueReport().then((response) => {
     store.pendingRescueReport = response
+  })
+  getPendingVolunteer().then((response) => {
+    console.log(response)
+
+    store.pendingVolunteer = response
   })
   const raw = sessionStorage.getItem('user_data')
   store.userData = raw ? JSON.parse(raw) : {}

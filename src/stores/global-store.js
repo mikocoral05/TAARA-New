d6c60287a4bfad94a7ce3bb0dcc2265064e45028
michael angelo <mikocoral05@gmail.com>
2025-05-18 18:10:10 +0800

    new global state

--- src/stores/global-store.js
+++ src/stores/global-store.js
@@ -10,6 +10,7 @@ export const globalStore = defineStore('store', {
     counter: 0,
     userData: {},
     pendingRescueReport: 0,
+    pendingVolunteer: 0,
   }),
   getters: {
     doubleCount: (state) => state.counter * 2,

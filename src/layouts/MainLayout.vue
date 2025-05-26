b71a5b9f58a948e7a15aac3be07a951ebcdcbc1e
michael angelo <mikocoral05@gmail.com>
2025-05-26 17:31:26 +0800

    cheange user managemnet to account maintenance

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -160,7 +160,7 @@ export default defineComponent({
         nav: '/management',
       },
       {
-        title: 'User Management',
+        title: 'Account Maintenance',
         icon: 'groups',
         nav: '/management/users',
         pending: store.pendingVolunteer,
@@ -237,7 +237,7 @@ export default defineComponent({
 
     const routeToTabMap = {
       '/management/': 'Dashboard',
-      '/management/users': 'User Management',
+      '/management/users': 'Account Maintenance',
       '/management/report': 'Report',
       '/management/pets': 'Pets',
       '/management/budget-and-expenses': 'Budget & Expenses',

670eb68c77b347f29bb25ead87c874c3f1f0f312
michael angelo <mikocoral05@gmail.com>
2025-05-27 19:32:40 +0800

    added table

--- src/pages/AnalyticReport.vue
+++ src/pages/AnalyticReport.vue
@@ -78,7 +78,17 @@
     </div>
     <div class="radius-10 q-mb-md row no-wrap">
       <DoughnutChart style="width: 300px" class="bg-white q-mr-md radius-10" />
-      <div class="bg-white q-pa-md full-width radius-10"></div>
+      <div class="bg-white full-width radius-10">
+        <q-table :rows="rows" :columns="columns" row-key="name" :rows-per-page-options="[4]" flat>
+          <template v-slot:top>
+            <div class="text-body1 q-mb-md">
+              Geographical Hotspots <q-icon class="q-ml-sm" size="1.2rem" name="sym_r_language" />
+            </div>
+          </template>
+        </q-table>
+
+        <LeafletMap v-if="showMap" />
+      </div>
     </div>
     <div class="bg-white radius-10">
       <StackBarLine />
@@ -87,16 +97,144 @@
 </template>
 <script>
 import DoughnutChart from 'src/components/DoughnutChart.vue'
+import LeafletMap from 'src/components/LeafletMap.vue'
 import StackBarLine from 'src/components/StackBarLine.vue'
 import { formatNumber, formatOrNumber } from 'src/composable/simpleComposable'
+import { ref } from 'vue'
 
 export default {
-  components: { StackBarLine, DoughnutChart },
+  components: { StackBarLine, DoughnutChart, LeafletMap },
   setup() {
-    return {
-      formatOrNumber,
-      formatNumber,
-    }
+    const columns = [
+      {
+        name: 'id',
+        required: true,
+        label: '#',
+        align: 'left',
+        field: (row) => row.name,
+        format: (val) => `${val}`,
+        sortable: true,
+      },
+      {
+        name: 'Address',
+        required: true,
+        label: 'Dessert (100g serving)',
+        align: 'left',
+        field: (row) => row.name,
+        format: (val) => `${val}`,
+        sortable: true,
+      },
+      { name: 'calories', align: 'center', label: 'Calories', field: 'calories', sortable: true },
+      { name: 'fat', label: 'Fat (g)', field: 'fat', sortable: true },
+    ]
+
+    const rows = [
+      {
+        id: 1,
+        name: 'Frozen Yogurt',
+        calories: 159,
+        fat: 6.0,
+        carbs: 24,
+        protein: 4.0,
+        sodium: 87,
+        calcium: '14%',
+        iron: '1%',
+      },
+      {
+        id: 2,
+        name: 'Ice cream sandwich',
+        calories: 237,
+        fat: 9.0,
+        carbs: 37,
+        protein: 4.3,
+        sodium: 129,
+        calcium: '8%',
+        iron: '1%',
+      },
+      {
+        id: 3,
+        name: 'Eclair',
+        calories: 262,
+        fat: 16.0,
+        carbs: 23,
+        protein: 6.0,
+        sodium: 337,
+        calcium: '6%',
+        iron: '7%',
+      },
+      {
+        name: 'Cupcake',
+        calories: 305,
+        fat: 3.7,
+        carbs: 67,
+        protein: 4.3,
+        sodium: 413,
+        calcium: '3%',
+        iron: '8%',
+      },
+      {
+        name: 'Gingerbread',
+        calories: 356,
+        fat: 16.0,
+        carbs: 49,
+        protein: 3.9,
+        sodium: 327,
+        calcium: '7%',
+        iron: '16%',
+      },
+      {
+        name: 'Jelly bean',
+        calories: 375,
+        fat: 0.0,
+        carbs: 94,
+        protein: 0.0,
+        sodium: 50,
+        calcium: '0%',
+        iron: '0%',
+      },
+      {
+        name: 'Lollipop',
+        calories: 392,
+        fat: 0.2,
+        carbs: 98,
+        protein: 0,
+        sodium: 38,
+        calcium: '0%',
+        iron: '2%',
+      },
+      {
+        name: 'Honeycomb',
+        calories: 408,
+        fat: 3.2,
+        carbs: 87,
+        protein: 6.5,
+        sodium: 562,
+        calcium: '0%',
+        iron: '45%',
+      },
+      {
+        name: 'Donut',
+        calories: 452,
+        fat: 25.0,
+        carbs: 51,
+        protein: 4.9,
+        sodium: 326,
+        calcium: '2%',
+        iron: '22%',
+      },
+      {
+        name: 'KitKat',
+        calories: 518,
+        fat: 26.0,
+        carbs: 65,
+        protein: 7,
+        sodium: 54,
+        calcium: '12%',
+        iron: '6%',
+      },
+    ]
+    const showMap = ref(false)
+    return { columns, rows, showMap, formatOrNumber, formatNumber }
   },
 }
 </script>

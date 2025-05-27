df3a647c5a3b7f33d351257da14aacd6cffc5964
michael angelo <mikocoral05@gmail.com>
2025-05-27 18:16:16 +0800

    udpate to analytic report

--- src/pages/AnalyticReport.vue
+++ src/pages/AnalyticReport.vue
@@ -27,7 +27,7 @@
                 <div class="text-grey-7 text-caption">PET AVAILABLE</div>
                 <q-icon name="sym_r_pets" size="1.5rem" color="primary" />
               </div>
-              <div class="text-h6 text-bold q-mt-lg">{{ formatOrNumber(4534534) }}</div>
+              <div class="text-h6 text-bold q-mt-lg">{{ formatOrNumber(4534) }}</div>
               <div class="text-grey-7 text-caption q-mt-sm">
                 <q-icon name="sym_r_south_west" class="text-negative text-bold q-mr-xs" /><span
                   class="text-negative text-bold"
@@ -43,7 +43,7 @@
                 <div class="text-grey-7 text-caption">TOTAL ADOPTED</div>
                 <q-icon name="sym_r_diversity_1" size="1.5rem" color="positive" />
               </div>
-              <div class="text-h6 text-bold q-mt-lg">{{ formatOrNumber(34734) }}</div>
+              <div class="text-h6 text-bold q-mt-lg">{{ formatOrNumber(3734) }}</div>
               <div class="text-grey-7 text-caption q-mt-sm">
                 <q-icon name="sym_r_north_east" class="text-positive text-bold q-mr-xs" /><span
                   class="text-positive text-bold"
@@ -59,7 +59,7 @@
                 <div class="text-grey-7 text-caption">IN MEDICATION</div>
                 <q-icon name="sym_r_monitor_heart" size="1.5rem" color="blue" />
               </div>
-              <div class="text-h6 text-bold q-mt-lg">{{ formatOrNumber(34734) }}</div>
+              <div class="text-h6 text-bold q-mt-lg">{{ formatOrNumber(1734) }}</div>
               <div class="text-grey-7 text-caption q-mt-sm">
                 <q-icon name="sym_r_north_east" class="text-positive text-bold q-mr-xs" /><span
                   class="text-positive text-bold"
@@ -76,15 +76,22 @@
         <q-btn icon="sym_r_upload" dense unelevated />
       </div>
     </div>
-    <div class="bg-white radius-10"><StackBarLine /></div>
+    <div class="radius-10 q-mb-md row no-wrap">
+      <DoughnutChart style="width: 300px" class="bg-white q-mr-md radius-10" />
+      <div class="bg-white q-pa-md full-width radius-10"></div>
+    </div>
+    <div class="bg-white radius-10">
+      <StackBarLine />
+    </div>
   </q-page>
 </template>
 <script>
+import DoughnutChart from 'src/components/DoughnutChart.vue'
 import StackBarLine from 'src/components/StackBarLine.vue'
 import { formatNumber, formatOrNumber } from 'src/composable/simpleComposable'
 
 export default {
-  components: { StackBarLine },
+  components: { StackBarLine, DoughnutChart },
   setup() {
     return {
       formatOrNumber,

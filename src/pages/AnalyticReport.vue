a892b5b139b09881327deb050f969763eff2a7b4
michael angelo <mikocoral05@gmail.com>
2025-05-28 13:50:25 +0800

    update two report

--- src/pages/AnalyticReport.vue
+++ src/pages/AnalyticReport.vue
@@ -1,129 +1,290 @@
 <template>
   <q-page>
-    <div class="row no-wrap justify-between items-start">
-      <div class="column no-wrap">
-        <div class="row q-mb-md">
-          <q-card class="q-mr-md radius-10 q-px-md" flat style="width: 250px">
-            <q-card-section>
-              <div class="row no-wrap justify-between">
-                <div class="text-grey-7 text-caption">OVERALL RESCUE</div>
-                <q-icon name="sym_r_fire_truck" size="1.5rem" color="red" />
-              </div>
-              <div class="text-h6 text-bold q-mt-lg">
-                {{ formatOrNumber(23235) }}
-              </div>
-              <div class="text-grey-7 text-caption q-mt-sm">
-                <q-icon name="sym_r_north_east" class="text-positive text-bold q-mr-xs" /><span
-                  class="text-positive text-bold"
-                  >25%</span
+    <div class="column no-wrap" v-if="tab == 1">
+      <div class="row no-wrap justify-between items-start">
+        <div class="column no-wrap">
+          <div class="row q-mb-md">
+            <q-card class="q-mr-md radius-10 q-px-md" flat style="width: 250px">
+              <q-card-section>
+                <div class="row no-wrap justify-between">
+                  <div class="text-grey-7 text-caption">OVERALL RESCUE</div>
+                  <q-icon name="sym_r_fire_truck" size="1.5rem" color="red" />
+                </div>
+                <div class="text-h6 text-bold q-mt-lg">
+                  {{ formatOrNumber(23235) }}
+                </div>
+                <div class="text-grey-7 text-caption q-mt-sm">
+                  <q-icon name="sym_r_north_east" class="text-positive text-bold q-mr-xs" /><span
+                    class="text-positive text-bold"
+                    >25%</span
+                  >
+                  vs last month
+                </div>
+              </q-card-section>
+            </q-card>
+            <q-card class="q-mr-md radius-10 q-px-md" flat style="width: 250px">
+              <q-card-section>
+                <div class="row no-wrap justify-between">
+                  <div class="text-grey-7 text-caption">PET AVAILABLE</div>
+                  <q-icon name="sym_r_pets" size="1.5rem" color="primary" />
+                </div>
+                <div class="text-h6 text-bold q-mt-lg">{{ formatOrNumber(4534) }}</div>
+                <div class="text-grey-7 text-caption q-mt-sm">
+                  <q-icon name="sym_r_south_west" class="text-negative text-bold q-mr-xs" /><span
+                    class="text-negative text-bold"
+                    >-5%</span
+                  >
+                  vs last month
+                </div>
+              </q-card-section>
+            </q-card>
+            <q-card class="q-mr-md radius-10 q-px-md" flat style="width: 250px">
+              <q-card-section>
+                <div class="row no-wrap justify-between">
+                  <div class="text-grey-7 text-caption">TOTAL ADOPTED</div>
+                  <q-icon name="sym_r_diversity_1" size="1.5rem" color="positive" />
+                </div>
+                <div class="text-h6 text-bold q-mt-lg">{{ formatOrNumber(3734) }}</div>
+                <div class="text-grey-7 text-caption q-mt-sm">
+                  <q-icon name="sym_r_north_east" class="text-positive text-bold q-mr-xs" /><span
+                    class="text-positive text-bold"
+                    >15%</span
+                  >
+                  vs last month
+                </div>
+              </q-card-section>
+            </q-card>
+            <q-card class="q-mr-md radius-10 q-px-md" flat style="width: 250px">
+              <q-card-section>
+                <div class="row no-wrap justify-between">
+                  <div class="text-grey-7 text-caption">IN MEDICATION</div>
+                  <q-icon name="sym_r_monitor_heart" size="1.5rem" color="blue" />
+                </div>
+                <div class="text-h6 text-bold q-mt-lg">{{ formatOrNumber(1734) }}</div>
+                <div class="text-grey-7 text-caption q-mt-sm">
+                  <q-icon name="sym_r_north_east" class="text-positive text-bold q-mr-xs" /><span
+                    class="text-positive text-bold"
+                    >15%</span
+                  >
+                  vs last month
+                </div>
+              </q-card-section>
+            </q-card>
+          </div>
+        </div>
+        <div class="row no-wrap justify-between items-center">
+          <q-btn icon="sym_r_download" dense unelevated />
+          <q-btn icon="sym_r_more_vert" dense unelevated>
+            <q-menu>
+              <q-list style="min-width: 160px">
+                <q-item
+                  clickable
+                  v-close-popup
+                  class="row no-wrap items-center"
+                  :active="tab == 1"
+                  @click="tab = 1"
                 >
-                vs last month
-              </div>
-            </q-card-section>
-          </q-card>
-          <q-card class="q-mr-md radius-10 q-px-md" flat style="width: 250px">
-            <q-card-section>
-              <div class="row no-wrap justify-between">
-                <div class="text-grey-7 text-caption">PET AVAILABLE</div>
-                <q-icon name="sym_r_pets" size="1.5rem" color="primary" />
-              </div>
-              <div class="text-h6 text-bold q-mt-lg">{{ formatOrNumber(4534) }}</div>
-              <div class="text-grey-7 text-caption q-mt-sm">
-                <q-icon name="sym_r_south_west" class="text-negative text-bold q-mr-xs" /><span
-                  class="text-negative text-bold"
-                  >-5%</span
+                  <q-item-section>Pet Report </q-item-section>
+                  <q-item-section avatar>
+                    <q-icon name="sym_r_pets" />
+                  </q-item-section>
+                </q-item>
+
+                <q-separator></q-separator>
+                <q-item
+                  clickable
+                  v-close-popup
+                  class="row no-wrap items-center"
+                  :active="tab == 2"
+                  @click="tab = 2"
                 >
-                vs last month
-              </div>
-            </q-card-section>
-          </q-card>
-          <q-card class="q-mr-md radius-10 q-px-md" flat style="width: 250px">
-            <q-card-section>
-              <div class="row no-wrap justify-between">
-                <div class="text-grey-7 text-caption">TOTAL ADOPTED</div>
-                <q-icon name="sym_r_diversity_1" size="1.5rem" color="positive" />
-              </div>
-              <div class="text-h6 text-bold q-mt-lg">{{ formatOrNumber(3734) }}</div>
-              <div class="text-grey-7 text-caption q-mt-sm">
-                <q-icon name="sym_r_north_east" class="text-positive text-bold q-mr-xs" /><span
-                  class="text-positive text-bold"
-                  >15%</span
-                >
-                vs last month
-              </div>
-            </q-card-section>
-          </q-card>
-          <q-card class="q-mr-md radius-10 q-px-md" flat style="width: 250px">
-            <q-card-section>
-              <div class="row no-wrap justify-between">
-                <div class="text-grey-7 text-caption">IN MEDICATION</div>
-                <q-icon name="sym_r_monitor_heart" size="1.5rem" color="blue" />
-              </div>
-              <div class="text-h6 text-bold q-mt-lg">{{ formatOrNumber(1734) }}</div>
-              <div class="text-grey-7 text-caption q-mt-sm">
-                <q-icon name="sym_r_north_east" class="text-positive text-bold q-mr-xs" /><span
-                  class="text-positive text-bold"
-                  >15%</span
-                >
-                vs last month
-              </div>
-            </q-card-section>
-          </q-card>
+                  <q-item-section>Budget Report </q-item-section>
+                  <q-item-section avatar>
+                    <q-icon name="sym_r_bar_chart" />
+                  </q-item-section>
+                </q-item>
+              </q-list>
+            </q-menu>
+          </q-btn>
         </div>
       </div>
-      <div class="row no-wrap justify-between items-center">
-        <q-btn icon="sym_r_download" dense unelevated />
-        <q-btn icon="sym_r_more_vert" dense unelevated>
-          <q-menu>
-            <q-list style="min-width: 160px">
-              <q-item clickable v-close-popup class="row no-wrap items-center">
-                <q-item-section>Pet Report </q-item-section>
-                <q-item-section avatar>
-                  <q-icon name="sym_r_pets" />
-                </q-item-section>
-              </q-item>
+      <div class="radius-10 q-mb-md row no-wrap">
+        <DoughnutChart style="width: 300px" class="bg-white q-mr-md radius-10" />
+        <div class="bg-white full-width radius-10 q-pa-md">
+          <q-table :rows="rows" :columns="columns" row-key="name" :rows-per-page-options="[4]" flat>
+            <template v-slot:top>
+              <div class="row no-wrap q-mb-md items-center">
+                <div class="text-body1">
+                  Geographical Hotspots
+                  <q-icon class="q-ml-sm" size="1.2rem" name="sym_r_language" />
+                </div>
+                <div class="text-grey-7 q-ml-md text-caption">
+                  Most frequently reported rescue locations
+                </div>
+              </div>
+            </template>
 
-              <q-separator></q-separator>
-              <q-item clickable v-close-popup class="row no-wrap items-center">
-                <q-item-section>Budget Report </q-item-section>
-                <q-item-section avatar>
-                  <q-icon name="sym_r_bar_chart" />
-                </q-item-section>
-              </q-item>
-            </q-list>
-          </q-menu>
-        </q-btn>
+            <template v-slot:body-cell-btn="props">
+              <q-td :props="props">
+                <q-btn flat dense no-caps :ripple="false" class="q-px-md">
+                  <span class="q-mr-sm text-caption">Show </span>
+                  <q-icon name="sym_r_location_on" size="1.2rem" class="cursor-pointer" />
+                </q-btn>
+              </q-td>
+            </template>
+          </q-table>
+        </div>
+      </div>
+      <div class="bg-white radius-10">
+        <StackBarLine title="Budget vs Expense" />
       </div>
     </div>
-    <div class="radius-10 q-mb-md row no-wrap">
-      <DoughnutChart style="width: 300px" class="bg-white q-mr-md radius-10" />
-      <div class="bg-white full-width radius-10 q-pa-md">
-        <q-table :rows="rows" :columns="columns" row-key="name" :rows-per-page-options="[4]" flat>
-          <template v-slot:top>
-            <div class="row no-wrap q-mb-md items-center">
-              <div class="text-body1">
-                Geographical Hotspots
-                <q-icon class="q-ml-sm" size="1.2rem" name="sym_r_language" />
-              </div>
-              <div class="text-grey-7 q-ml-md text-caption">
-                Most frequently reported rescue locations
-              </div>
-            </div>
-          </template>
+    <div class="column no-wrap" v-if="tab == 2">
+      <div class="row no-wrap justify-between items-start">
+        <div class="column no-wrap">
+          <div class="row q-mb-md">
+            <q-card class="q-mr-md radius-10 q-px-md" flat style="width: 250px">
+              <q-card-section>
+                <div class="row no-wrap justify-between">
+                  <div class="text-grey-7 text-caption">AVAILABLE BUDGET</div>
+                  <q-icon name="sym_r_fire_truck" size="1.5rem" color="red" />
+                </div>
+                <div class="text-h6 text-bold q-mt-lg">
+                  {{ formatOrNumber(23235) }}
+                </div>
+                <div class="text-grey-7 text-caption q-mt-sm">
+                  <q-icon name="sym_r_north_east" class="text-positive text-bold q-mr-xs" /><span
+                    class="text-positive text-bold"
+                    >25%</span
+                  >
+                  vs last month
+                </div>
+              </q-card-section>
+            </q-card>
+            <q-card class="q-mr-md radius-10 q-px-md" flat style="width: 250px">
+              <q-card-section>
+                <div class="row no-wrap justify-between">
+                  <div class="text-grey-7 text-caption">TOTAL EXPENSE</div>
+                  <q-icon name="sym_r_pets" size="1.5rem" color="primary" />
+                </div>
+                <div class="text-h6 text-bold q-mt-lg">{{ formatOrNumber(4534) }}</div>
+                <div class="text-grey-7 text-caption q-mt-sm">
+                  <q-icon name="sym_r_south_west" class="text-negative text-bold q-mr-xs" /><span
+                    class="text-negative text-bold"
+                    >-5%</span
+                  >
+                  vs last month
+                </div>
+              </q-card-section>
+            </q-card>
+            <q-card class="q-mr-md radius-10 q-px-md" flat style="width: 250px">
+              <q-card-section>
+                <div class="row no-wrap justify-between">
+                  <div class="text-grey-7 text-caption">REMAINING BALANCE</div>
+                  <q-icon name="sym_r_diversity_1" size="1.5rem" color="positive" />
+                </div>
+                <div class="text-h6 text-bold q-mt-lg">{{ formatOrNumber(3734) }}</div>
+                <div class="text-grey-7 text-caption q-mt-sm">
+                  <q-icon name="sym_r_north_east" class="text-positive text-bold q-mr-xs" /><span
+                    class="text-positive text-bold"
+                    >15%</span
+                  >
+                  vs last month
+                </div>
+              </q-card-section>
+            </q-card>
+            <!-- <q-card class="q-mr-md radius-10 q-px-md" flat style="width: 250px">
+              <q-card-section>
+                <div class="row no-wrap justify-between">
+                  <div class="text-grey-7 text-caption">IN MEDICATION</div>
+                  <q-icon name="sym_r_monitor_heart" size="1.5rem" color="blue" />
+                </div>
+                <div class="text-h6 text-bold q-mt-lg">{{ formatOrNumber(1734) }}</div>
+                <div class="text-grey-7 text-caption q-mt-sm">
+                  <q-icon name="sym_r_north_east" class="text-positive text-bold q-mr-xs" /><span
+                    class="text-positive text-bold"
+                    >15%</span
+                  >
+                  vs last month
+                </div>
+              </q-card-section>
+            </q-card> -->
+          </div>
+        </div>
+        <div class="row no-wrap justify-between items-center">
+          <q-btn icon="sym_r_download" dense unelevated />
+          <q-btn icon="sym_r_more_vert" dense unelevated>
+            <q-menu>
+              <q-list style="min-width: 160px">
+                <q-item
+                  clickable
+                  v-close-popup
+                  class="row no-wrap items-center"
+                  :active="tab == 1"
+                  @click="tab = 1"
+                >
+                  <q-item-section>Pet Report </q-item-section>
+                  <q-item-section avatar>
+                    <q-icon name="sym_r_pets" />
+                  </q-item-section>
+                </q-item>
 
-          <template v-slot:body-cell-btn="props">
-            <q-td :props="props">
-              <q-btn flat dense no-caps :ripple="false" class="q-px-md">
-                <span class="q-mr-sm text-caption">Show </span>
-                <q-icon name="sym_r_location_on" size="1.2rem" class="cursor-pointer" />
-              </q-btn>
-            </q-td>
-          </template>
-        </q-table>
+                <q-separator></q-separator>
+                <q-item
+                  clickable
+                  v-close-popup
+                  class="row no-wrap items-center"
+                  :active="tab == 2"
+                  @click="tab = 2"
+                >
+                  <q-item-section>Budget Report </q-item-section>
+                  <q-item-section avatar>
+                    <q-icon name="sym_r_bar_chart" />
+                  </q-item-section>
+                </q-item>
+              </q-list>
+            </q-menu>
+          </q-btn>
+        </div>
       </div>
-    </div>
-    <div class="bg-white radius-10">
-      <StackBarLine />
+
+      <div class="bg-white radius-10">
+        <StackBarLine
+          title="Budget vs Expense"
+          :chartLabels="[
+            'January',
+            'February',
+            'March',
+            'April',
+            'May',
+            'June',
+            'July',
+            'August',
+            'September',
+            'October',
+            'November',
+            'December',
+          ]"
+          :chartDatasets="[
+            {
+              label: 'Revenue',
+              data: [100, 120, 150, 180, 200, 220, 250, 270, 290, 310, 330, 350],
+              borderColor: 'rgb(75, 192, 192)',
+              backgroundColor: 'rgba(75, 192, 192, 0.5)',
+              stack: 'combined',
+              type: 'bar',
+            },
+            {
+              label: 'Profit',
+              data: [50, 60, 70, 90, 110, 130, 140, 160, 170, 180, 190, 200],
+              borderColor: 'rgb(255, 99, 132)',
+              backgroundColor: 'rgba(255, 99, 132, 0.5)',
+              stack: 'combined',
+              type: 'line',
+            },
+          ]"
+        />
+      </div>
     </div>
   </q-page>
 </template>
@@ -131,6 +292,7 @@
 import DoughnutChart from 'src/components/DoughnutChart.vue'
 import StackBarLine from 'src/components/StackBarLine.vue'
 import { formatNumber, formatOrNumber } from 'src/composable/simpleComposable'
+import { ref } from 'vue'
 
 export default {
   components: { StackBarLine, DoughnutChart },
@@ -148,7 +310,7 @@ export default {
       {
         name: 'address',
         required: true,
-        label: 'Address',
+        label: 'Full Address',
         align: 'left',
         field: 'address',
         sortable: true,
@@ -162,8 +324,8 @@ export default {
       { id: 3, address: 'Barangay Lidong, Sto. Domingo, Albay' },
       { id: 4, address: 'Barangay Buhatan, Sto. Domingo, Albay' },
     ]
-
-    return { columns, rows, formatOrNumber, formatNumber }
+    const tab = ref(2)
+    return { tab, columns, rows, formatOrNumber, formatNumber }
   },
 }
 </script>

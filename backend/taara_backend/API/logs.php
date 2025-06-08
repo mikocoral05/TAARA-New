93c086871ef464ec2a128903ff78849015c45422
michael angelo <mikocoral05@gmail.com>
2025-06-08 15:17:03 +0800

    new lgos api

---
+++ backend/taara_backend/API/logs.php
@@ -0,0 +1,67 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Credentials: true");
+header("Access-Control-Allow-Methods: POST, GET, PUT, DELETE");
+header("Access-Control-Allow-Headers: Content-Type");
+require_once('../MysqliDb.php');
+
+class API
+{
+    public function __construct()
+    {
+        $this->db = new MysqliDB('localhost', 'root', '', 'capstone');
+    }
+
+    public function httpGet($payload)
+    {
+        if (array_key_exists('logs', $payload)) {
+            $query = $this->db->get('tbl_logs');
+
+            echo json_encode([
+                'status' => 'success',
+                'data' => $query,
+                'method' => 'GET'
+            ]);
+        } else {
+            echo json_encode([
+                'status' => 'failed',
+                'message' => 'A error occured while performing action!'
+            ]);
+        }
+    }
+
+
+    public function httpPost($payload) {}
+
+    public function httpPut($payload) {}
+
+
+    public function httpDelete($payload) {}
+}
+
+/* END OF CLASS */
+
+
+$received_data = json_decode(file_get_contents('php://input'), true);
+
+$request_method = $_SERVER['REQUEST_METHOD'];
+
+
+$api = new API;
+
+
+if ($request_method == 'GET') {
+    $api->httpGet($_GET);
+}
+
+if ($request_method == 'POST') {
+    $api->httpPost($received_data);
+}
+
+if ($request_method == 'PUT') {
+    $api->httpPut($received_data);
+}
+
+if ($request_method == 'DELETE') {
+    $api->httpDelete($received_data);
+}

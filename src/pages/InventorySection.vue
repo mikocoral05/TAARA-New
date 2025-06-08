7a8703036b80db35313caecb30ac2f852dbaa808
michael angelo <mikocoral05@gmail.com>
2025-06-08 21:15:52 +0800

    inveontroey ydpate

--- src/pages/InventorySection.vue
+++ src/pages/InventorySection.vue
@@ -574,7 +574,7 @@ export default {
     })
     const preventAction = () => {
       const userType = store.userData.user_type
-      const userRole = store.userData.role
+      const userRole = store.userData.roles
       const official = [1, 2, 3, 4].includes(userRole) && userType == 3
       const volunteer = [1, 2].includes(userRole) && userType == 2
       const result = userType == 3 ? official : volunteer

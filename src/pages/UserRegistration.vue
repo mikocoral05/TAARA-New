4dae49b1d2f0bd8b68cc492af5f7632276354dc8
michael angelo <mikocoral05@gmail.com>
2025-06-09 16:56:39 +0800

    useregostratin udpate

--- src/pages/UserRegistration.vue
+++ src/pages/UserRegistration.vue
@@ -121,7 +121,7 @@
                       <template v-slot:append>
                         <q-icon name="event" class="cursor-pointer">
                           <q-popup-proxy cover transition-show="scale" transition-hide="scale">
-                            <q-date v-model="registrationDetails.birth_date">
+                            <q-date v-model="registrationDetails.birth_date" mask="YYYY-MM-DD">
                               <div class="row items-center justify-end">
                                 <q-btn v-close-popup label="Close" color="primary" flat />
                               </div>

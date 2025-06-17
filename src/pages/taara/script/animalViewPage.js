c2ce53079498aa2d64cf87d848921efa6001c75f
michael angelo <mikocoral05@gmail.com>
2025-06-17 20:31:21 +0800

    asdf

--- src/pages/taara/script/animalViewPage.js
+++ src/pages/taara/script/animalViewPage.js
@@ -1,13 +1,10 @@
-import { ref, onMounted, watch, onUnmounted } from 'vue'
+import { ref, onMounted, watch } from 'vue'
 import TaaraFooter from 'src/components/TaaraFooter.vue'
 import { useCounterStore } from 'src/stores/example-store'
-import { createWorker } from 'tesseract.js'
 import { useRoute, useRouter } from 'vue-router'
 import { globalStore } from 'src/stores/global-store'
 import {
   dateToday,
-  timeNow,
-  resizeImage,
   decodeAnimalId,
   encodeAnimalId,
   getImageLink,
@@ -37,13 +34,6 @@ export default {
     const route = useRoute()
     const router = useRouter()
     const counterStore = useCounterStore()
-    let showPorgressAndOther = ref(false)
-    let contactedQStepper = ref(false)
-    let donateDialog = ref(false)
-    let imageOrReference = ref(true)
-    let donationImage = ref(null)
-    let reference = ref(null)
-    let controlSpinner = ref(false)
     let inFront = ref([])
     let step = ref(1)
     const textNoAccount = ref('')
@@ -52,16 +42,6 @@ export default {
     const specificAnimal = ref({})
     const allAnimalData6 = ref([])
     const likesData = ref([])
-    let donatorsInfo = ref({
-      donators_id: null,
-      animal_id: null,
-      donation_amount: null,
-      image: null,
-      reference: null,
-      donation_date: null,
-      donation_time: null,
-      donation_status: 1,
-    })
 
     let rearrangeImage = (button) => {
       let temp = []
@@ -136,10 +116,6 @@ export default {
       return false
     }
 
-    const handleWindowResize = () => {
-      contactedQStepper.value = window.innerWidth < 779
-    }
-
     watch(
       () => route.query.pet,
       (newVal) => {
@@ -180,68 +156,7 @@ export default {
       }
     }
 
-    watch(
-      () => [donationImage.value, reference.value],
-      () => {
-        ;(async () => {
-          let obj = {
-            donators_id: store.userData.user_id,
-            animal_id: specificAnimal.value.data[0].animal_id,
-            donation_amount: null,
-            image: null,
-            reference: null,
-            donation_date: dateToday,
-            donation_time: timeNow,
-            donation_status: 1,
-          }
-          if (donationImage.value == null) {
-            obj.reference = 'Ref No. ' + reference.value
-            obj.image = null
-            obj.donation_amount = null
-          } else {
-            controlSpinner.value = true
-            const reader = new FileReader()
-            reader.readAsDataURL(donationImage.value)
-            reader.onload = () => {
-              resizeImage(donationImage.value, 500, 500)
-                .then(({ dataUrl }) => {
-                  obj.image = dataUrl
-                })
-                .catch((error) => {
-                  console.error(error)
-                })
-            }
-            const worker = await createWorker('eng')
-            const {
-              data: { text },
-            } = await worker.recognize(donationImage.value)
-            const amountRegex = /(Total Amount Sent|Amount|Php)\s*(\d+(\.\d{1,2})?)/gi
-
-            let match
-            const amounts = []
-            while ((match = amountRegex.exec(text)) !== null) {
-              amounts.push(match[2]) // This will push only the numerical amounts to the array
-              console.log(match)
-            }
-            donationImage.value !== null ? (obj.donation_amount = Number(amounts[0])) : null
-            const refNoRegex = /Ref No\.\s*([\d\s]+)/gi // Adjusted to match the format of the reference number
-            let refMatch
-            if ((refMatch = refNoRegex.exec(text)) !== null) {
-              // This will set the reference number in the object
-              donationImage.value !== null
-                ? (obj.reference = refMatch[1])
-                : (obj.reference = reference.value)
-            }
-            controlSpinner.value = false
-            await worker.terminate()
-          }
-          donatorsInfo.value = { ...obj }
-        })()
-      },
-    )
-
     onMounted(async () => {
-      window.addEventListener('resize', handleWindowResize)
       viewSpecificAnimal(decodeAnimalId(route.query.pet)).then((response) => {
         specificAnimal.value = response
       })
@@ -261,9 +176,7 @@ export default {
       }
       moreAnimalForAdoption()
     })
-    onUnmounted(() => {
-      window.removeEventListener('resize', handleWindowResize)
-    })
+
     return {
       adoptBtnFn,
       noAccount,
@@ -273,20 +186,13 @@ export default {
       getImageLink,
       store,
       encodeAnimalId,
-      donatorsInfo,
       submitPublicDonation,
-      controlSpinner,
-      donationImage,
-      reference,
-      imageOrReference,
-      donateDialog,
       accountActive,
       counterStore,
       getAge,
       switchCase,
       step,
       like,
-      showPorgressAndOther,
       getSubmitAdoptionForm,
       adoptedAnimalOnProgress,
       likeFn,
@@ -298,7 +204,6 @@ export default {
       viewSpecificAnimal,
       inFront,
       specificAnimal,
-      contactedQStepper,
       options: [
         {
           value: 'True',

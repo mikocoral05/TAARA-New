4067986428db6f94571a6bf391ae2702784b49c2
michael angelo <mikocoral05@gmail.com>
2025-06-17 17:02:58 +0800

    asdf

--- src/pages/AnnouncementsPagePublic.vue
+++ src/pages/AnnouncementsPagePublic.vue
@@ -5,81 +5,119 @@
       style="background: rgba(240, 242, 245, 255)"
     >
       <div
-        v-for="data in annoucementData.data"
-        :key="data.announcement_id"
+        v-for="(data, index) in annoucementData"
+        :key="index"
         class="q-mb-xl q-mt-sm q-pa-xl announcements-container"
-        :id="'report-' + data.announcement_id"
+        :id="'report-' + data.id"
       >
-        <div class="flex row justify-between items-center big-announcement-title-container">
-          <div class="flex row justify-center items-center q-px-md">
-            <div class="flex column justify-start items-start q-ma-sm announcement-title-container">
-              <h4 class="q-ma-none q-pa-none announcement-title">Announcements</h4>
+        <div class="row no-wrap justify-between items-center big-announcement-title-container">
+          <div class="row no-wrap items-center">
+            <q-avatar>
+              <img :src="data?.user_image_path || 'https://cdn.quasar.dev/img/avatar.png'" />
+            </q-avatar>
+            <div class="column no-wrap q-ml-md">
+              <div class="text-bold">Tabaco Animal Rescue and Adoption</div>
+              <div class="text-grey-7">{{ data.created_at }}</div>
             </div>
           </div>
-          <div class="flex column justify-center items-center q-pr-md date-and-time-container">
-            <p class="q-ma-sm">{{ wordifyDate(data.date) }}</p>
-            <p>{{ wordifyTime(data.time) }}</p>
+          <div class="row no-wrap">
+            <q-icon name="sym_r_keep" class="q-mr-md" color="green" size="1.3rem" />
+            Pinned by Taara
           </div>
         </div>
-        <div class="q-ma-sm flex row no-wrap image-container">
-          <div
-            v-for="(image, index) in annoucementData.image
-              .filter((image) => data.announcement_id === image.announcement_id)
-              .splice(0, 5)"
-            :key="image.id"
-            @click="(viewImage(index, data.announcement_id), (carousel = true))"
-          >
-            <img
-              v-if="data.announcement_id === image.announcement_id"
-              class="animal_image q-ma-sm"
-              :src="image.image"
-              alt=""
-            />
-          </div>
-          <q-dialog v-model="carousel" maximized>
-            <q-carousel swipeable animated arrows v-model="slide" infinite dark>
-              <q-carousel-slide
-                v-for="(image, index) in annoucementData.image
-                  .filter((image) => qDialogImageId === image.announcement_id)
-                  .splice(0, 5)"
-                :key="image.id"
-                :name="index"
-                class="flex justify-center items-center"
-              >
-                <q-icon
-                  name="close"
-                  class="absolute-top-right color-white q-ma-md close-dialog"
-                  @click="carousel = false"
-                />
-                <img
-                  v-if="qDialogImageId === image.announcement_id"
-                  :src="image.image"
-                  alt=""
-                  style="max-width: 100%; height: auto"
-                />
-              </q-carousel-slide>
-            </q-carousel>
-          </q-dialog>
+        <div
+          class="q-ma-sm flex row no-wrap image-container q-mt-lg"
+          v-if="data?.announcement_image_path"
+        >
+          <q-img class="radius-10" :src="data.announcement_image_path" :ratio="16 / 9" />
         </div>
-        <div>
-          <p class="q-ma-md announcement-details" v-if="showMore == true">
-            <TextClamp :max-lines="5" :text="data.announcement_details">
-              <template #after="{ toggle, expanded }">
-                <q-btn class="showLessMoreButton" flat dense @click="toggle">
-                  {{ expanded ? 'Show less' : 'Show more' }}
-                </q-btn>
-              </template>
-            </TextClamp>
-          </p>
-          <p class="q-ma-md announcement-details" v-else-if="showMore == false">
-            {{ data.announcement_details }}
-          </p>
+        <div class="column no-wrap q-mt-lg">
+          <div class="text-body1 text-bold">{{ data.title }}</div>
+          <div v-html="data.content"></div>
         </div>
       </div>
     </div>
-    <PageFooter />
+    <TaaraFooter />
   </q-page>
 </template>
 
-<script src="../pages/taara/script/announcementsPage"></script>
+<script>
+import { onMounted, ref, watch } from 'vue'
+import { timeAgo, wordifyDate, wordifyTime } from 'src/composable/simpleComposable'
+import { useCounterStore } from 'src/stores/example-store'
+import { getAllAnnouncement } from 'src/composable/latestComposable'
+import TaaraFooter from 'src/components/TaaraFooter.vue'
+export default {
+  components: {
+    TaaraFooter,
+  },
+
+  setup() {
+    const counterStore = useCounterStore()
+    const myElement = ref(null)
+    const annoucementData = ref([])
+    let textLength = ref(150)
+    const scrollToElement = () => {
+      setTimeout(() => {
+        const element = document.querySelector(`#report-${counterStore.announcement}`)
+        if (element) {
+          element.scrollIntoView({ behavior: 'smooth' })
+        }
+      }, 10)
+    }
+
+    let showMore = ref(false)
+    const handleWindowResize = () => {
+      if (window.innerWidth < 415) {
+        showMore.value = true
+      } else {
+        showMore.value = false
+      }
+    }
+
+    function toggleShowMore(item) {
+      item.showMore = !item.showMore
+    }
+    let slide = ref(1)
+    let qDialogImageId = ref()
+    let viewImage = (payload, id) => {
+      slide.value = payload
+      qDialogImageId.value = id
+    }
+
+    window.addEventListener('resize', handleWindowResize)
+    watch(
+      () => counterStore.announcement,
+      (newValue, oldValue) => {
+        console.log(newValue + 'd' + oldValue)
+        scrollToElement()
+      },
+    )
+    onMounted(async () => {
+      handleWindowResize()
+      annoucementData.value = await getAllAnnouncement()
+      console.log(annoucementData.value)
+
+      setTimeout(() => {
+        scrollToElement()
+      }, 1000)
+    })
+    return {
+      timeAgo,
+      qDialogImageId,
+      viewImage,
+      toggleShowMore,
+      wordifyDate,
+      wordifyTime,
+      getAllAnnouncement,
+      annoucementData,
+      myElement,
+      showMore,
+      textLength,
+      slide,
+      carousel: ref(false),
+    }
+  },
+}
+</script>
 <style lang="scss" scoped src="../pages/taara/style/announcementsPage.scss"></style>

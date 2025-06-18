c849c4d54474d944148a6ace2dee3f737a82d738
michael angelo <mikocoral05@gmail.com>
2025-06-18 19:22:11 +0800

    dfsf

--- backend/taara_backend/API/budget_expenses.php
+++ backend/taara_backend/API/budget_expenses.php
@@ -298,14 +298,14 @@ class API
                 $logs = [
                     'user_id' => $user_id,
                     'user_type' => $user_type,
-                    'action' => 'Archieve List from ' . $tab,
+                    'action' => 'Archive List from ' . $tab,
                     'module' => 'Budget and Expenses',
                 ];
 
                 $this->db->insert("tbl_logs", $logs);
-                echo json_encode(['status' => 'success', 'message' => 'Records soft-deleted successfully', 'method' => 'PUT']);
+                echo json_encode(['status' => 'success', 'message' => 'Records Archive successfully', 'method' => 'PUT']);
             } else {
-                echo json_encode(['status' => 'error', 'message' => 'Failed to soft delete records', 'method' => 'PUT']);
+                echo json_encode(['status' => 'error', 'message' => 'Failed to Archive records', 'method' => 'PUT']);
             }
         } else {
             // Handle the case where 'updateUser' key is missing

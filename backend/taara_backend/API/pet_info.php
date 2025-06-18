013ba17cfa7899367b3563ab97ba12cd4853f330
michael angelo <mikocoral05@gmail.com>
2025-06-18 19:22:24 +0800

    asdf

--- backend/taara_backend/API/pet_info.php
+++ backend/taara_backend/API/pet_info.php
@@ -152,13 +152,13 @@ class API
                 $logs = [
                     'user_id' => $user_id,
                     'user_type' => $user_type,
-                    'action' => 'Archieve pet record',
+                    'action' => 'Archive pet record',
                     'module' => 'Pet Info',
                 ];
                 $this->db->insert("tbl_logs", $logs);
-                echo json_encode(['status' => 'success', 'message' => 'Records soft-deleted successfully', 'method' => 'PUT']);
+                echo json_encode(['status' => 'success', 'message' => 'Records Archive successfully', 'method' => 'PUT']);
             } else {
-                echo json_encode(['status' => 'error', 'message' => 'Failed to soft delete records', 'method' => 'PUT']);
+                echo json_encode(['status' => 'error', 'message' => 'Failed to Archive records', 'method' => 'PUT']);
             }
         } else if (isset($payload['edit_animal_info'])) {
             $obj = $payload['edit_animal_info']['animal_data'];

4c8e9be9d4c395824d2bfccd27f1e17d51fe7fb0
michael angelo <mikocoral05@gmail.com>
2025-06-18 14:29:26 +0800

    newa dded ipdtiosn

--- src/composable/optionsComposable.js
+++ src/composable/optionsComposable.js
@@ -39,3 +39,34 @@ export const sexOption = ref([
 ])
 
 export const petSizes = ['Small', 'Medium', 'Large', 'Giant']
+
+export const daysOptions = [
+  {
+    label: 'Monday',
+    value: 'Monday',
+  },
+  {
+    label: 'Tuesday',
+    value: 'Tuesday',
+  },
+  {
+    label: 'Wednesday',
+    value: 'Wednesday',
+  },
+  {
+    label: 'Thursday',
+    value: 'Thursday',
+  },
+  {
+    label: 'Friday',
+    value: 'Friday',
+  },
+  {
+    label: 'Saturday',
+    value: 'Saturday',
+  },
+  {
+    label: 'Sunday',
+    value: 'Sunday',
+  },
+]
